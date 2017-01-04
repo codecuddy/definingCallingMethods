1e898fee93bc9e1539fe05e7b72dfaed933afcc4
Charlie Cuddy <charliecuddy4@gmail.com>
2017-01-04 01:15:39 -0600

    Add project files.

---
+++ definingCallingMethods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace definingCallingMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HelloWorld();
+            GoodByeWorld();
+            Console.ReadLine();
+        }
+
+        private static void HelloWorld()
+        {
+            Console.WriteLine("hello world");
+        }
+
+        private static void GoodByeWorld()
+        {
+            Console.WriteLine("goodbye world");
+        }
+    }
+}
